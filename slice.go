fc8da8902aa6c25f5db4df216b5e1dd6c01b0842
Amit Kundu <kundu.amit517@gmail.com>
2022-05-09 12:14:06 +0600

    assignment ongoing

---
+++ slice.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+var input string
+
+func main() {
+	s := make([]int, 1, 3)
+
+	// loop that runs infinitely
+	for {
+
+		if number > 5 {
+			break
+		}
+
+		fmt.Printf("%d\n", number)
+		number++
+
+	}
+}
+
+func takeUserInput() {
+	fmt.Println("Please enter a number: ")
+	fmt.Scan(&input)
+}
