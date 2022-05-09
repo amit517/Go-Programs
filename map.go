05c1a287d8a76c46104fac3875be68d47dc71280
Amit Kundu <kundu.amit517@gmail.com>
2022-05-09 10:44:07 +0600

    some more operation on Map

--- map.go
+++ map.go
@@ -5,7 +5,24 @@ import (
 )
 
 func main() {
-	var idMap map[string]int
-	idMap = make(map[string]int)
+	/* 	var idMap map[string]int
+	   	idMap = make(map[string]int) */
 
+	idMap := map[string]int{
+		"joe": 420}
+
+	fmt.Println(idMap["joe"])
+
+	idMap["amit"] = 265
+	fmt.Println("my id is", idMap["amit"])
+	/* 	delete(idMap, "amit")
+	   	fmt.Println("my id is", idMap["amit"]) */
+
+	id, p := idMap["joe"]
+	fmt.Println("my id is", id)
+	fmt.Println("the value of p is", p)
+
+	for key, value := range idMap {
+		fmt.Println("key ", key, "value ", value)
+	}
 }
